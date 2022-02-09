ec40a35bd745673ff092ec9d8f5f744fd695ed80
zhy801005 <zhy801005@gmail.com>
2022-02-09 21:09:17 +0800

    首页卖车列表

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,4 +1,8 @@
 <?php
+
+use \Tools\Tools;
+use \Mysql\Common\CommonModel as MysqlCommon;
+
 class IndexController extends \Base\Controller_AbstractWechat
 {
     public function indexAction()
@@ -7,7 +11,13 @@ class IndexController extends \Base\Controller_AbstractWechat
 
     public function getSellListAction()
     {
-        
+        $list = $brandList = MysqlCommon::getInstance()->getListByTableName('car_sell_list', ['title', 'pic_dir',
+            'register_date', 'mileage', 'sell_price'], ['is_home'=>1,'status'=>1], 'id desc');
+
+        $res['code'] = 200;
+        $res['data'] = $list;
+
+        Tools::returnAjaxJson($res);
     }
 
 
