764b1b33152135fe57ca3be90bd942532876605f
yanjiayu <yanjiayu@yanjiayudeMacBook-Pro.local>
2022-03-24 15:45:14 +0800

    新闻分类

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -7,8 +7,14 @@ class NewsController extends \Base\Controller_AbstractWechat
 {
     public function newsAction()
     {
-        $news = MysqlCommon::getInstance()->getListByTableName("car_news",['id','title', 'content', 'update_time', 'type'],['status'=>1],"id DESC");
-        $this->getView()->assign(['news'=>$news]);
+        $type = $this->getRequest()->getQuery("t");
+        if (!empty($type) && is_numeric($type)) {
+            $news = MysqlCommon::getInstance()->getListByTableName("car_news",['id','title', 'content', 'update_time', 'type'],['status'=>1, 'type'=>$type],"id DESC");
+        } else {
+            $news = MysqlCommon::getInstance()->getListByTableName("car_news",['id','title', 'content', 'update_time', 'type'],['status'=>1],"id DESC");
+        }
+
+        $this->getView()->assign(['news'=>$news, 'here'=>$type]);
     }
 
     public function contentAction()
